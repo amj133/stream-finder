beb9d5102d4ac79124cfa97474b420a76925b72f
amj133 <amj@vt.edu>
2018-04-03 20:57:36 -0600

    write user_can_log_in_spec

---
+++ spec/features/user_can_log_in_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+feature "user can log in" do
+  context "visits root" do
+    describe "clicks on sign in" do
+      let(:user) { create(:user) }
+
+      it "fills out information, clicks sign in, brought to their dashboard" do
+        visit "/"
+        click_on("Sign in")
+
+        expect(current_path).to eq("/login")
+
+        fill_in("name", with: "Billy")
+        fill_in("password", with: "password")
+        click_on("Sign in")
+
+        expect(current)path).to eq("/users/billy")
+      end
+    end
+  end
+end
