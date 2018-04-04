dbaf168ca8ca925aed9d44046bc0908bb147b0b5
amj133 <amj@vt.edu>
2018-04-03 20:49:20 -0600

    add to visitor can register spec - not with duplicate name and email

--- spec/features/visitor_can_register_spec.rb
+++ spec/features/visitor_can_register_spec.rb
@@ -37,6 +37,44 @@ feature "visitor can register" do
          expect(current_path).to eq("/users")
          expect(User.count).to eq(0)
        end
+
+       it "does not register if name is taken" do
+         create(:user)
+         visit root_path
+
+         click_on("Sign up!")
+
+         expect(current_path).to eq("/register")
+
+         fill_in("Name", with: "Billy")
+         fill_in("Email", with: "BillyRocks@example.com")
+         fill_in("Password", with: "password")
+         fill_in("Password confirmation", with: "password")
+         click_on("Sign up")
+
+         expect(current_path).to_not eq("/users/billy")
+         expect(current_path).to eq("/users")
+         expect(User.count).to eq(1)
+       end
+
+       it "does not register if email is taken" do
+         create(:user)
+         visit root_path
+
+         click_on("Sign up!")
+
+         expect(current_path).to eq("/register")
+
+         fill_in("Name", with: "Billy123")
+         fill_in("Email", with: "billy@example.com")
+         fill_in("Password", with: "password")
+         fill_in("Password confirmation", with: "password")
+         click_on("Sign up")
+
+         expect(current_path).to_not eq("/users/billy")
+         expect(current_path).to eq("/users")
+         expect(User.count).to eq(1)
+       end
     end
   end
 end
