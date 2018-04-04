c33e6ecc023dce08a7e4fffd4dc242981cba7240
amj133 <amj@vt.edu>
2018-04-04 07:25:05 -0600

    create AddAttributesToUsers migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180404002635) do
+ActiveRecord::Schema.define(version: 20180404132222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,10 @@ ActiveRecord::Schema.define(version: 20180404002635) do
     t.string "slug"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "uid"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "token"
   end
 
 end
