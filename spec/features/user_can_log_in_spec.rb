0eaf5ae00b0bccdbad9246dbd015d23ca8a2507f
amj133 <amj@vt.edu>
2018-04-03 21:18:27 -0600

    add cannot log in with incorrect name or password to user can login spec

--- spec/features/user_can_log_in_spec.rb
+++ spec/features/user_can_log_in_spec.rb
@@ -19,6 +19,38 @@ feature "user can log in" do
 
         expect(current_path).to eq("/users/billy")
       end
+
+      it "cannot log in with incorrect name" do
+        user
+
+        visit "/"
+        click_on("Sign in")
+
+        expect(current_path).to eq("/login")
+
+        fill_in("name", with: "Billy123")
+        fill_in("password", with: "password")
+        click_on("Sign in")
+
+        expect(current_path).to_not eq("/users/billy")
+        expect(current_path).to eq("/login")
+      end
+
+      it "cannot log in with incorrect password" do
+        user
+
+        visit "/"
+        click_on("Sign in")
+
+        expect(current_path).to eq("/login")
+
+        fill_in("name", with: "Billy")
+        fill_in("password", with: "wrong")
+        click_on("Sign in")
+
+        expect(current_path).to_not eq("/users/billy")
+        expect(current_path).to eq("/login")
+      end
     end
   end
 end
