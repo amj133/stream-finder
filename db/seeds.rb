be696770d5dcff64526cb6773c06c7bd6f7e1e74
amj133 <amj@vt.edu>
2018-04-05 12:08:43 -0600

    create seed file and co_stations_fixture

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+stations = CSV.open('/db/fixtures/stations_fixture.csv', headers: true, header_converters: :symbol)
+stations.each do |row|
+  Station.create!(
+    organization: row[:organization],
+    location_id: row[:location_id],
+    location_name: row[:location_name],
+    location_type: row[:location_type],
+    huc_8: row[:huc_8],
+    drainage_area_value: row[:drainage_area_value],
+    drainage_area_unit_code: row[:drainage_area_unit_code],
+    contributing_drainage_area_value: row[:contributing_drainage_area_value],
+    latitude: row[:latitude],
+    longitude: row[:longitude],
+    horizontal_datum: row[:horizontal_datum],
+    vertical_datum: row[:vertical_datum],
+    state_code: row[:state_code],
+    county_code: row[:county_code],
+    aquifer_name: row[:aquifer_name],
+    formation_type: row[:formation_type],
+    aquifer_type: row[:aquifer_type],
+    construction_date: row[:construction_date],
+    well_depth_value: row[:well_depth_value],
+    well_depth_unit_code: row[:well_depth_unit_code],
+    provider: row[:provider]
+  )
+end
