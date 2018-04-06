4ddab7462be57b7696d661a07397e0748e0065eb
amj133 <amj@vt.edu>
2018-04-05 18:57:35 -0600

    write service/station_spec

---
+++ spec/services/station_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe Station do
+  let(:attrs) {
+    {
+      "MonitoringLocationIdentity"=>
+      {"MonitoringLocationIdentifier"=>"USGS-09251500",
+       "MonitoringLocationName"=>"MF LITTLE SNAKE RIVER NEAR BATTLE CREEK, CO.",
+       "MonitoringLocationTypeName"=>"Stream",
+       "HUCEightDigitCode"=>"14050003",
+       "DrainageAreaMeasure"=>{"MeasureValue"=>"120", "MeasureUnitCode"=>"sq mi"}},
+       "MonitoringLocationGeospatial"=>
+         {"LatitudeMeasure"=>"40.9905222", "LongitudeMeasure"=>"-107.0442186"}
+        }
+  }
+  subject { Station.new(attrs) }
+
+  it "exists" do
+    expect(subject).to be_a(Station)
+  end
+
+  it "assings attributes to reader methods" do
+    expect(subject.id).to eq("USGS-09251500")
+    expect(subject.name).to eq("MF LITTLE SNAKE RIVER NEAR BATTLE CREEK, CO.")
+    expect(subject.type).to eq("Stream")
+    expect(subject.description).to eq("Stream")
+    expect(subject.drainage_area).to eq("120")
+    expect(subject.drainage_area_units).to eq("sq mi")
+    expect(subject.latitude).to eq("40.9905222")
+    expect(subject.longitude).to eq("-107.0442186")
+  end
+end
