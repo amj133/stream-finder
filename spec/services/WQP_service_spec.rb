4d783677369136ac683f90d9fba4793cf9e764c3
amj133 <amj@vt.edu>
2018-04-06 19:59:56 -0600

    #station_by_id block added to WQP_service_spec

--- spec/services/WQP_service_spec.rb
+++ spec/services/WQP_service_spec.rb
@@ -1,28 +1,63 @@
 require 'rails_helper'
 
 describe WQPService do
-  it "has a collection of raw_station data" do
-    VCR.use_cassette("WQP service station by huc search") do
-      params = {"huc_code" => "10190017"}
-      search = WQPService.new(params).station_by_huc
+  describe "instance methods" do
+    describe "#station_by_huc" do
+      it "has a collection of raw_station data" do
+        VCR.use_cassette("WQP service station by huc search") do
+          params = {"huc_code" => "10190017"}
+          search = WQPService.new(params).station_by_huc
 
-      expect(search).to have_key("WQX")
-      expect(search["WQX"]).to be_a(Hash)
+          expect(search).to have_key("WQX")
+          expect(search["WQX"]).to be_a(Hash)
 
-      expect(search["WQX"]).to have_key("Organization")
-      expect(search["WQX"]["Organization"]).to be_a(Array)
+          expect(search["WQX"]).to have_key("Organization")
+          expect(search["WQX"]["Organization"]).to be_a(Array)
 
-      expect(search["WQX"]["Organization"][0]).to have_key("MonitoringLocation")
-      expect(search["WQX"]["Organization"][0]["MonitoringLocation"]).to be_a(Array)
+          expect(search["WQX"]["Organization"][0]).to have_key("MonitoringLocation")
+          expect(search["WQX"]["Organization"][0]["MonitoringLocation"]).to be_a(Array)
 
-      raw_station = search["WQX"]["Organization"][0]["MonitoringLocation"].first
+          raw_station = search["WQX"]["Organization"][0]["MonitoringLocation"].first
 
-      expect(raw_station).to be_a(Hash)
-      expect(raw_station).to have_key("MonitoringLocationIdentity")
-      expect(raw_station["MonitoringLocationIdentity"]).to be_a(Hash)
+          expect(raw_station).to be_a(Hash)
+          expect(raw_station).to have_key("MonitoringLocationIdentity")
+          expect(raw_station["MonitoringLocationIdentity"]).to be_a(Hash)
 
-      expect(raw_station["MonitoringLocationIdentity"]).to have_key("MonitoringLocationName")
-      expect(raw_station["MonitoringLocationIdentity"]["MonitoringLocationName"]).to be_a(String)
+          expect(raw_station["MonitoringLocationIdentity"]).to have_key("MonitoringLocationName")
+          expect(raw_station["MonitoringLocationIdentity"]["MonitoringLocationName"]).to be_a(String)
+        end
+      end
+    end
+
+    describe "#station_by_id" do
+      it "returns a single raw station" do
+        VCR.use_cassette("WQP service station by id search") do
+          params = {"station_id" => "USGS-09251500"}
+          raw_station = WQPService.new(params).station_by_id
+
+          expect(raw_station).to have_key("WQX")
+          expect(raw_station["WQX"]).to be_a(Hash)
+
+          expect(raw_station["WQX"]["Organization"]).to be_a(Hash)
+
+          expect(raw_station["WQX"]["Organization"]["OrganizationDescription"]["OrganizationFormalName"]).to be_a(String)
+
+          expect(raw_station["WQX"]["Organization"]["MonitoringLocation"]).to be_a(Hash)
+          expect(raw_station["WQX"]["Organization"]["MonitoringLocation"]).to be_a(Hash)
+          expect(raw_station["WQX"]["Organization"]["MonitoringLocation"]["MonitoringLocationIdentity"]).to be_a(Hash)
+          expect(raw_station["WQX"]["Organization"]["MonitoringLocation"]["MonitoringLocationIdentity"]["MonitoringLocationIdentifier"]).to be_a(String)
+          expect(raw_station["WQX"]["Organization"]["MonitoringLocation"]["MonitoringLocationIdentity"]["MonitoringLocationName"]).to be_a(String)
+          expect(raw_station["WQX"]["Organization"]["MonitoringLocation"]["MonitoringLocationIdentity"]["MonitoringLocationTypeName"]).to be_a(String)
+          expect(raw_station["WQX"]["Organization"]["MonitoringLocation"]["MonitoringLocationIdentity"]["HUCEightDigitCode"]).to be_a(String)
+          expect(raw_station["WQX"]["Organization"]["MonitoringLocation"]["MonitoringLocationIdentity"]["DrainageAreaMeasure"]).to be_a(Hash)
+          expect(raw_station["WQX"]["Organization"]["MonitoringLocation"]["MonitoringLocationIdentity"]["DrainageAreaMeasure"]["MeasureValue"]).to be_a(String)
+          expect(raw_station["WQX"]["Organization"]["MonitoringLocation"]["MonitoringLocationIdentity"]["DrainageAreaMeasure"]["MeasureUnitCode"]).to be_a(String)
+
+          expect(raw_station["WQX"]["Organization"]["MonitoringLocation"]["MonitoringLocationGeospatial"]).to be_a(Hash)
+          expect(raw_station["WQX"]["Organization"]["MonitoringLocation"]["MonitoringLocationGeospatial"]["LatitudeMeasure"]).to be_a(String)
+          expect(raw_station["WQX"]["Organization"]["MonitoringLocation"]["MonitoringLocationGeospatial"]["LongitudeMeasure"]).to be_a(String)
+        end
+      end
     end
   end
 end
