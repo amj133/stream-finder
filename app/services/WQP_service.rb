1d1e37ebb74455763f06ce983a9006f7749a7bbf
amj133 <amj@vt.edu>
2018-04-06 20:01:19 -0600

    add #station_by_id method to WQP_service - spec passing

--- app/services/WQP_service.rb
+++ app/services/WQP_service.rb
@@ -3,19 +3,25 @@ class WQPService
 
   def initialize(attrs = {})
     @huc_code = attrs["huc_code"]
+    @station_id = attrs["station_id"]
   end
 
-  def station_by_huc
+  def station_by_huc #change to stations!!!
+    params = base_params.merge!({"huc" => huc_code})
+    xml_response("/data/Station/search", params)
+  end
+
+  def station_by_id
+    params = base_params.merge!({"siteid" => station_id})
     xml_response("/data/Station/search", params)
   end
 
   private
-    attr_reader :huc_code
+    attr_reader :huc_code, :station_id
 
-    def params # make base_params & do params = base_params.merge(additional)
+    def base_params
       {
-        "state" => "US:08",
-        "huc" => huc_code
+        "state" => "US:08"
       }
     end
 
