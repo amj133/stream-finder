4316f76d3ddfea8708884f97c8867d08c9f64add
amj133 <amj@vt.edu>
2018-04-08 19:02:41 -0600

    create geojsoN_builder model, spec passing

---
+++ app/models/geojson_builder.rb
@@ -0,0 +1,22 @@
+class GeojsonBuilder
+
+  def build_station(station)
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [station.longitude, station.latitude] # this part is tricky
+      },
+      properties: {
+        name: station.name,
+        type: station.type,
+        drainage_area: station.drainage_area,
+        drainage_area_units: station.drainage_area_units,
+        :"marker-color" => "#FFFFFF",
+        :"marker-symbol" => "circle",
+        :"marker-size" => "medium",
+      }
+    }
+  end
+
+end
