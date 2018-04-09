00e59d4ace256f64d7bc3f5440b1c1b0e79ea932
amj133 <amj@vt.edu>
2018-04-09 13:04:55 -0600

    allow station index to respond with json format

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -3,16 +3,32 @@ class StationsController < ApplicationController
 
   def index
     @co_watershed_hucs ||= CO_WATERSHED_HUCS
-    @stations = StationsByHUC.new(params[:huc_code]).stations if params[:huc_code]
+    if params[:huc_code]
+      @stations = StationsByHUC.new(params[:huc_code]).stations
+      @geojson = []
+      transform_to_geojson(@stations, @geojson)
+    end
+    
+    respond_to do |format|
+      format.html
+      format.json { render json: @geojson}
+    end
   end
 
   def show
     @station = StationByID.new(params[:id]).station
   end
 
+
   private
     def require_current_user
       render file: "public/404" unless current_user
+    end
+
+    def transform_to_geojson(stations, geojson)
+      stations.each do |station|
+        geojson << GeojsonBuilder.build_station(station)
+      end
     end
 
 end
