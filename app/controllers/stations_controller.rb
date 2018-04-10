658482dc2d2d7077de359008743c72f6bc48c04f
amj133 <amj@vt.edu>
2018-04-10 14:09:00 -0600

    allow transform to geojson to return geojson array

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -2,13 +2,13 @@ class StationsController < ApplicationController
   before_action :require_current_user
 
   def index
-    @co_watershed_hucs ||= CO_WATERSHED_HUCS
+    @co_watershed_hucs = CO_WATERSHED_HUCS
     if params[:huc_code]
       @stations = StationsByHUC.new(params[:huc_code]).stations
       @geojson = []
-      transform_to_geojson(@stations, @geojson)
+      @points = transform_to_geojson(@stations, @geojson)
     end
-    
+
     respond_to do |format|
       format.html
       format.json { render json: @geojson}
@@ -29,6 +29,7 @@ class StationsController < ApplicationController
       stations.each do |station|
         geojson << GeojsonBuilder.build_station(station)
       end
+      geojson
     end
 
 end
