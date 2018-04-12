15912976ac4290121fb480d8af9db9d9efd78c8f
amj133 <amj@vt.edu>
2018-04-12 17:32:19 -0600

    change time series model and spec to convert time to datetime

--- app/models/time_series.rb
+++ app/models/time_series.rb
@@ -2,8 +2,17 @@ class TimeSeries
   attr_reader :time, :discharge
 
   def initialize(time_collection, discharge_collection)
-    @time = time_collection
-    @discharge = discharge_collection.map {|q| q.to_i}
+    @time_as_string = time_collection
+    @discharge = discharge_collection.map {|q| q.to_f}
   end
+
+  def time
+    time_as_string.map do |time|
+      DateTime.strptime(time, '%Y-%m-%dT%H:%M')
+    end
+  end
+
+  private
+    attr_reader :time_as_string
 
 end
--- spec/models/time_series_spec.rb
+++ spec/models/time_series_spec.rb
@@ -1,13 +1,26 @@
 require 'rails_helper'
 
 describe TimeSeries do
-  let(:time) { [1, 2, 3, 4, 5] }
+  let(:time) { [
+      "2018-04-11T16:30:00.000-05:00",
+      "2018-04-11T16:45:00.000-05:00",
+      "2018-04-11T17:00:00.000-05:00",
+      "2018-04-11T17:15:00.000-05:00",
+      "2018-04-11T17:30:00.000-05:00",
+    ] }
   let(:discharge) { [11.2, 11.3, 11.3, 11.2, 11.6] }
   subject { TimeSeries.new(time, discharge) }
 
   it "sets attributes as reader methods" do
     expect(subject).to be_a(TimeSeries)
-    expect(subject.time).to eq(time)
+
+    first_datetime = DateTime.strptime("2018-04-11T16:30:00.000-05:00", '%Y-%m-%dT%H:%M')
+
+    expect(subject.time).to be_a(Array)
+    expect(subject.time.first).to be_a(DateTime)
+    expect(subject.time.first).to eq(first_datetime)
+
     expect(subject.discharge).to eq(discharge)
+    expect(subject.discharge.first).to eq(11.2)
   end
 end
