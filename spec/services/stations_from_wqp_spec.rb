a4929a6f1bea5df86b7ef1095436beec2e127844
amj133 <amj@vt.edu>
2018-04-11 19:53:11 -0600

    add returns a single station to stations_from_wqp_spec

--- spec/services/stations_from_wqp_spec.rb
+++ spec/services/stations_from_wqp_spec.rb
@@ -4,6 +4,8 @@ describe StationsFromWQP do
   context "instance methods" do
     describe "#stations" do
       it "returns a collection of stations" do
+        stations_by_many_params_stub
+
         params = {
           "huc" => "14080201",
           "statecode" => "US:08",
@@ -14,7 +16,20 @@ describe StationsFromWQP do
         expect(stations.first).to be_a(Station)
         expect(stations.last).to be_a(Station)
 
-        expect(stations.first).to eq(5)
+        expect(stations.first.id).to eq("USGS-09371010")
+        expect(stations.last.id).to eq("UTEMTN-SJ-4C")
+      end
+
+      it "returns a single station" do
+        station_by_id_stub
+
+        params = { "siteid" => "USGS-09251500" }
+
+        station = StationsFromWQP.new(params).stations
+
+        expect(station).to be_a(Station)
+        expect(station.id).to eq("USGS-09251500")
+        expect(station.longitude).to eq("-107.0442186")
       end
     end
   end
