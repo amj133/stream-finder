586e656ff3df910cb4689b8a13399c1335868efe
amj133 <amj@vt.edu>
2018-04-11 19:54:11 -0600

    allow stations_from_wqp to parse response with multiple and single organization

--- app/services/stations_from_wqp.rb
+++ app/services/stations_from_wqp.rb
@@ -7,8 +7,10 @@ class StationsFromWQP
 
   def stations
     response = raw_stations
-    parse_array(response).map do |raw_station|
-      Station.new(raw_station)
+    if response["WQX"]["Organization"].class == Array
+      stations_from_multiple_organizations(response)
+    elsif response["WQX"]["Organization"].class == Hash
+      stations_from_single_organization(response)
     end
   end
 
@@ -17,6 +19,17 @@ class StationsFromWQP
 
     def raw_stations
       WQPService.new(search_params).stations
+    end
+
+    def stations_from_multiple_organizations(response)
+      parse_array(response).map do |raw_station|
+        Station.new(raw_station)
+      end
+    end
+
+    def stations_from_single_organization(response)
+      station_attrs = response["WQX"]["Organization"]["MonitoringLocation"]
+      Station.new(station_attrs)
     end
 
 end
