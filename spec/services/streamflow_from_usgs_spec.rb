11b44d9c426abb847a6e4f3d4fb98ef072bdf497
amj133 <amj@vt.edu>
2018-04-12 17:35:16 -0600

    update streamflow from usgs to reflect datetime change in timeseries poro

--- spec/services/streamflow_from_usgs_spec.rb
+++ spec/services/streamflow_from_usgs_spec.rb
@@ -11,11 +11,11 @@ describe StreamflowFromUSGS do
         expect(time_series).to be_a(TimeSeries)
 
         expect(time_series.time).to be_a(Array)
-        expect(time_series.time.first).to eq("2018-04-11T16:30:00.000-05:00")
-        expect(time_series.time.last).to eq("2018-04-12T15:45:00.000-05:00")
+        expect(time_series.time.first).to be_a(DateTime)
+        expect(time_series.time.last).to be_a(DateTime)
 
         expect(time_series.discharge).to be_a(Array)
-        expect(time_series.discharge.first).to eq(10)
+        expect(time_series.discharge.first).to eq(10.3)
         expect(time_series.discharge.last).to eq(264)
       end
     end
