963abd6436989a9ae9c7083c2d14463d3692d693
amj133 <amj@vt.edu>
2018-04-14 14:29:00 -0600

    create user_sees_saved_stations_on_project_show_spec

---
+++ spec/features/user_sees_saved_stations_on_project_show_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe "user visits project show page" do
+  it "displays list of saved stations" do
+    user = create(:user)
+    project = create(:project, user: user)
+    station_1 = create(:station, project: project)
+    station_2 = create(:station, project: project)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(:user)
+
+    visit project_path(project)
+
+    expect(current_path).to eq("projects/project-1")
+    expect(page).to have_content("Saved Stations")
+    expect(page).to have_link("Station-1")
+    expect(page).to have_link("Station-2")
+  end
+end
