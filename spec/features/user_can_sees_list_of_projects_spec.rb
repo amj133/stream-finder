4a6fc72033efe0801659bfac5832d6a3a1f325f9
amj133 <amj@vt.edu>
2018-04-14 13:57:10 -0600

    add user projects to user dashboard, user can see list of projects spec passing

--- spec/features/user_can_sees_list_of_projects_spec.rb
+++ spec/features/user_can_sees_list_of_projects_spec.rb
@@ -4,14 +4,18 @@ feature "user sees list of projects" do
   context "visits their dashboard" do
     it "display list of existing projects" do
       user = create(:user)
-      project = create(:project, user: user)
+      project_1 = create(:project, user: user)
+      project_2 = create(:project, user: user)
 
-      visit user_path(user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit user_path(user.slug)
 
       expect(page).to have_content("Current Projects")
       expect(page).to have_link("Project 1")
+      expect(page).to have_content("Watershed HUC: 01234567")
       expect(page).to have_link("Project 2")
-      expect(page).to have_link("Project 3")
+      expect(page).to have_content("Watershed HUC: 01234567")
     end
   end
 end
