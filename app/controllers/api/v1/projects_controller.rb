499b1d179c17f7b084217a05490af8b6764ed89e
amj133 <amj@vt.edu>
2018-04-15 15:03:04 -0600

    create station to favorite station module, include module in station from wqp,solidfy create action of api/v1/projects controller - spec passing

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,7 +1,19 @@
 class Api::V1::ProjectsController < ApplicationController
 
   def create
-    binding.pry
+    project = current_user.projects.find_by_name(params[:project])
+    params[:fav_stations].each do |station|
+      search_params = {"siteid" => station}
+      project.favorite_stations.push(StationsFromWQP.new(search_params).favorite_station)
+    end
   end
+
+  # private
+  #   def get_station(fav_stations)
+  #     fav_stations.map do |station|
+  #       search_params = {"siteid" => station}
+  #       StationsFromWQP.new(search_params).stations
+  #     end
+  #   end
 
 end
