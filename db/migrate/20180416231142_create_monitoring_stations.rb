68375141592f445eb473ad58b87aa4e0fbb7af16
amj133 <amj@vt.edu>
2018-04-16 17:12:25 -0600

    create MonitoringStation model and migration

---
+++ db/migrate/20180416231142_create_monitoring_stations.rb
@@ -0,0 +1,16 @@
+class CreateMonitoringStations < ActiveRecord::Migration[5.1]
+  def change
+    create_table :monitoring_stations do |t|
+      t.string :org_id
+      t.string :name
+      t.string :type
+      t.text :description
+      t.float :latitude
+      t.float :longitude
+      t.float :drainage_area
+      t.string :drainage_area_units
+
+      t.timestamps
+    end
+  end
+end
