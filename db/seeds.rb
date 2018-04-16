2814ecd65b8e79d01b71daeaa7a2c8d748d23afb
amj133 <amj@vt.edu>
2018-04-16 17:23:16 -0600

    create db seed to populate MonitoringStations table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,45 @@
-stations = CSV.open('/db/fixtures/stations_fixture.csv', headers: true, header_converters: :symbol)
-stations.each do |row|
-  Station.create!(
-    organization: row[:organization],
-    location_id: row[:location_id],
-    location_name: row[:location_name],
-    location_type: row[:location_type],
-    huc_8: row[:huc_8],
-    drainage_area_value: row[:drainage_area_value],
-    drainage_area_unit_code: row[:drainage_area_unit_code],
-    contributing_drainage_area_value: row[:contributing_drainage_area_value],
-    latitude: row[:latitude],
-    longitude: row[:longitude],
-    horizontal_datum: row[:horizontal_datum],
-    vertical_datum: row[:vertical_datum],
-    state_code: row[:state_code],
-    county_code: row[:county_code],
-    aquifer_name: row[:aquifer_name],
-    formation_type: row[:formation_type],
-    aquifer_type: row[:aquifer_type],
-    construction_date: row[:construction_date],
-    well_depth_value: row[:well_depth_value],
-    well_depth_unit_code: row[:well_depth_unit_code],
-    provider: row[:provider]
-  )
-end
+hucs = [
+  "10190001",
+  "10190002",
+  "10190003",
+  "10190004",
+  "10190005",
+  "10190006",
+  "10190007",
+  "10190008",
+  "10190010",
+  "10190014",
+  "11020001",
+  "11020002",
+  "11020003",
+  "11020004",
+  "11020005",
+  "11020008",
+  "14010001",
+  "14010002",
+  "14010003"
+]
+
+desc "Loading all stations along urban front range"
+  task load_urban_stations: :environment do
+    MonitoringStation.destroy_all
+    puts 'Loading urban stations'
+
+    hucs.each do |huc|
+      stations = StationsFromWQP.new({"huc" => huc})
+      stations.each do |station|
+        MonitoringStation.create!(
+          org_id: station.id,
+          name: station.name,
+          type_of: station.type,
+          description: station.description,
+          latitude: station.latitude,
+          longitude: station.longitude,
+          drainage_area: station.drainage_area,
+          drainage_area_units: station.drainage_area_units
+        )
+      end
+    end
+
+    puts "All urban stations loaded\n"
+  end
