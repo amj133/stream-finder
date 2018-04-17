6757c4ef219c7da3102660f6f6a596c558190602
amj133 <amj@vt.edu>
2018-04-17 14:02:29 -0600

    refactor api project controller to have add_stations private method

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -2,20 +2,20 @@ class Api::V1::ProjectsController < ApplicationController
 
   def create
     project = current_user.projects.find_by_name(params[:project])
-    params[:fav_stations].each do |station|
-      search_params = {siteid: station}
-
-      # station = StationsPresenter.new(search_params).stations_by_id
-      binding.pry
-      project.favorite_stations.push(get_station(search_params))
-      # project.favorite_stations.push(StationsFromWQP.new(search_params).favorite_station)
-    end
+    stations_to_add = params[:fav_stations]
+    add_stations(project, stations_to_add)
+    # params[:fav_stations].each do |station|
+    #   search_params = {siteid: station}
+    #   project.favorite_stations.push(StationsFromWQP.new(search_params).favorite_station)
   end
 
   private
 
-    def get_station(search_params)
-      StationsPresenter.new(search_params).station_by_id
+    def add_stations(project, stations_to_add)
+      stations_to_add.each do |station|
+        search_params = {siteid: station}
+        project.favorite_stations.push(StationsFromWQP.new(search_params).favorite_station)
+      end
     end
 
 end
