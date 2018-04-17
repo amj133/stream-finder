821c1c01032a06c2102402209f5a1b381161889d
amj133 <amj@vt.edu>
2018-04-17 11:20:34 -0600

    write first block for #map search class method

--- spec/models/stream_station_spec.rb
+++ spec/models/stream_station_spec.rb
@@ -27,5 +27,22 @@ describe StreamStation do
         expect(station.org_id).to eq("USGS-2")
       end
     end
+
+    context "#map_search" do
+      it "returns collection of stream stations by search params" do
+        create(:stream_station, org_id: "USGS-1", huc: "1")
+        create(:stream_station, org_id: "USGS-2", huc: "2")
+        create(:stream_station, org_id: "USGS-3", huc: "1")
+
+        stations = StreamStation.map_search({"huc" => "1"})
+
+        expect(stations[0].org_id).to eq("USGS-1")
+        expect(stations[1].org_id).to eq("USGS-3")
+
+        station = StreamStation.map_search({"huc" => "2"})
+
+        expect(stations[0].org_id).to eq("USGS-2")
+      end
+    end
   end
 end
