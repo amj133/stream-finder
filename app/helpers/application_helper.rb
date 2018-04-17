4d23ec8108eefe4068d330c75e3a8e6636b91f05
amj133 <amj@vt.edu>
2018-04-17 16:05:52 -0600

    change search index layer show to remove comma

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,2 @@
 module ApplicationHelper
-  def cache_key_for(name, klass)
-    table_name = klass.to_s.pluralize.downcase
-    [table_name, name, klass.maximum(:updated_at), klass.count].join("-")
-  end
 end
