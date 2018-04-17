493b916d7923d8ba499e2ee80c2664cf523e0685
amj133 <amj@vt.edu>
2018-04-17 13:13:54 -0600

    create migration for stream_station_projects

--- app/models/stream_station.rb
+++ app/models/stream_station.rb
@@ -1,4 +1,10 @@
 class StreamStation < ApplicationRecord
+  validates_presence_of :org_id
+  validates_presence_of :type_of
+  validates_presence_of :latitude
+  validates_presence_of :longitude
+  # has_many :favorite_station_projects
+  # has_many :projects, through: :favorite_station_projects
 
   def self.by_huc(huc)
     where(huc: huc)
