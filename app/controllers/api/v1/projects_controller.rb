9d8b77718647090e2b6707e89942246a9ab4dd47
amj133 <amj@vt.edu>
2018-04-17 13:04:19 -0600

    begin updating create action of api v1 projects controller to be compatible with stations presenter

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -3,17 +3,19 @@ class Api::V1::ProjectsController < ApplicationController
   def create
     project = current_user.projects.find_by_name(params[:project])
     params[:fav_stations].each do |station|
-      search_params = {"siteid" => station}
-      project.favorite_stations.push(StationsFromWQP.new(search_params).favorite_station)
+      search_params = {siteid: station}
+
+      # station = StationsPresenter.new(search_params).stations_by_id
+      binding.pry
+      project.favorite_stations.push(get_station(search_params))
+      # project.favorite_stations.push(StationsFromWQP.new(search_params).favorite_station)
     end
   end
 
-  # private
-  #   def get_station(fav_stations)
-  #     fav_stations.map do |station|
-  #       search_params = {"siteid" => station}
-  #       StationsFromWQP.new(search_params).stations
-  #     end
-  #   end
+  private
+
+    def get_station(search_params)
+      StationsPresenter.new(search_params).station_by_id
+    end
 
 end
