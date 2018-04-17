b264ac066621d0496fe3f38cdee6a58e2b5829e3
amj133 <amj@vt.edu>
2018-04-17 11:42:08 -0600

    add class method by_type

--- spec/models/stream_station_spec.rb
+++ spec/models/stream_station_spec.rb
@@ -28,6 +28,19 @@ describe StreamStation do
       end
     end
 
+    context "#by_type" do
+      it "returns collection by type" do
+        create(:stream_station, org_id: "USGS-1", type_of: "Stream")
+        create(:stream_station, org_id: "USGS-2", type_of: "Well")
+        create(:stream_station, org_id: "USGS-3", type_of: "Stream")
+
+        stations = StreamStation.by_type("Stream")
+
+        expect(stations[0].org_id).to eq("USGS-1")
+        expect(stations[1].org_id).to eq("USGS-3")
+      end
+    end
+
     context "#map_search" do
       it "returns collection of stream stations by search params" do
         create(:stream_station, org_id: "USGS-1", huc: "1")
