09a6ed43152a1a31bd51ed56abaf2c6dc5266098
amj133 <amj@vt.edu>
2018-04-18 08:18:39 -0600

    redirect if flash to prevent persisting flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,13 +11,13 @@ class SessionsController < ApplicationController
       redirect_to user_path(user.slug)
     elsif user && user.password_digest.nil?
       flash[:notice] = "Must login through Google!"
-      render :new
+      redirect_to login_path
     elsif user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to user_path(user.slug)
     else
       flash[:notice] = "Incorrect credentials"
-      render :new
+      redirect_to login_path
     end
   end
 
