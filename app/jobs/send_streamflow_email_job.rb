e9f7f482d99f2600854d6d604eede7c818965a07
amj133 <amj@vt.edu>
2018-06-20 13:48:21 -0600

    update streamflow mailer to include site id in beginning of email

--- app/jobs/send_streamflow_email_job.rb
+++ app/jobs/send_streamflow_email_job.rb
@@ -1,7 +1,7 @@
 class SendStreamflowEmailJob < ApplicationJob
   queue_as :default
 
-  def perform(email, time, discharge)
-    StreamflowMailer.historical_streamflow(email, time, discharge).deliver_now
+  def perform(email, time, discharge, site_id)
+    StreamflowMailer.historical_streamflow(email, time, discharge, site_id).deliver_now
   end
 end
