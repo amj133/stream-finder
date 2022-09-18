0b3f14a4d92e7e2cda138d63fff18d1a517f68aa
amj133 <amj@vt.edu>
2022-09-18 13:44:59 -0400

    validate historic streamflow datetime params

--- app/controllers/api/v1/date_range_streamflow_controller.rb
+++ app/controllers/api/v1/date_range_streamflow_controller.rb
@@ -2,14 +2,25 @@ class Api::V1::DateRangeStreamflowController < ApplicationController
 
   def show
     search_params = {"site" => params[:id], "startDT" => params[:startDate], "endDT" => params[:endDate]}
-    streamflow = StreamflowFromUSGS.new(search_params).streamflow
+
+    begin
+      validate_datetime_params
+      streamflow = StreamflowFromUSGS.new(search_params).streamflow
+    rescue InvalidParameterError => e
+      response = {}
+      response[:errors] = e.api_error_messages
+
+      return render :json => response, :status => 400
+    end
 
     render json: streamflow
   end
 
   def download
     search_params = {"site" => params[:id], "startDT" => params[:startDate], "endDT" => params[:endDate] }
+
     begin
+      validate_datetime_params
       streamflow = StreamflowFromUSGS.new(search_params).streamflow
     rescue InvalidParameterError => e
       response = {}
@@ -27,6 +38,12 @@ class Api::V1::DateRangeStreamflowController < ApplicationController
 
   def date_range_streamflow_params
     params.permit(:start_date, :end_date)
+  end
+
+  def validate_datetime_params
+    if params[:startDate].match(/^\d{4}-\d{2}-\d{2}$/).nil? || params[:endDate].match(/^\d{4}-\d{2}-\d{2}$/).nil?
+      raise InvalidParameterError.new("Start and End Date must be in format 'YYYY-MM-DD'")
+    end
   end
 
 end
